c872a94ad8105f9c57776d9f4a54a086c2b44f0a
TEQUIE835 <david123orjuela@gmail.com>
2025-04-28 08:29:52 -0500

    si

--- Ejercicio3.py
+++ Ejercicio3.py
@@ -5,12 +5,10 @@ print("-" * 40)
 while True:
     try:
         mult=int(input("\nIngrese un numero del 1 al 10: "))
-        if 0<mult<=10:
-            break
-        else:print("\nIngrese un numero valido")
+        break
     except: print("\nIngrese un numero valido")
 
 print("\nEsta es la tabla de multiplicar:\n")
 for i in range(1,11):
     mul=mult*i
-    print(mul)
+    print(f"{mult} * {i} = ", mul)
