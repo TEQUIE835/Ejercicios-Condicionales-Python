0770f0af10315ed8a0ad6dd2e7bd3decf13ba2a5
TEQUIE835 <david123orjuela@gmail.com>
2025-04-28 08:20:41 -0500

    si

--- Ejercicio5.py
+++ Ejercicio5.py
@@ -7,23 +7,28 @@ print("-" * 40)
 numa=random.randint(1, 10)
 intentos= 3
 
-
+print(f"\nTienes {intentos} intentos")
 while intentos > 0:
-    print(f"Tienes {intentos} intentos")
-    while True:
-        try:
+    
+    
+    try:
             numu=int(input("\nIngrese un numero del 1 al 10: "))
             if 0<numu<=10:
-                break
-            else: print("\nIngrese un numero valido")
-        except: print("\nIngrese numero valido")
+                pass
+            else: 
+                print("\nIngrese un numero valido")
+                continue
+    except: print("\nIngrese numero valido")
     if numu==numa: 
         print("\nHas adivinado el numero")
         break
     elif numu<numa:
         print("\nEl numero es mayor")
+        intentos = intentos-1
     elif numu>numa:
         print("\nEl numero es menor")
+        intentos = intentos-1
+    print(f"\nTienes {intentos} intentos")
 
 
 if intentos>0:
